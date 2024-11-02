927d2d4a9c1e58fa3a712764024d9b039c7b7f35
0xChijioke <chijokeogbuanya@gmail.com>
2024-11-02 17:59:45 +0100

    events page and config

---
+++ packages/nextjs/app/events/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import EventComponent from '~~/components/vault/EventComponent';
+
+const EventPage = () => {
+  return (
+    <div>
+        <EventComponent />
+    </div>
+  )
+}
+
+export default EventPage;
